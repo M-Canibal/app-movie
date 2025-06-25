ccca1538ef72f42ed81662da39879ad35ba500b3
Matheus Canibal <mcpires@ps.grupopanvel.com.br>
2025-06-25 16:50:04 -0300

    outro-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import MovieDetail from './pages/MovieDetail';
 import MovieListPage from './pages/MovieListPage';
+import Footer from './components/footer';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -11,25 +12,26 @@ function App() {
   return (
     <>
       <Navbar onSearch={setSearchTerm} />
-      <Routes>
-        <Route path="/" element={<Home searchTerm={searchTerm} />} />
-        <Route path="/movie/:id" element={<MovieDetail />} />
-        <Route path="/populares" element={<MovieListPage title="Populares" endpoint="/movie/popular" searchTerm={searchTerm} />} />
-
-        <Route
-          path="/lancamentos"
-          element={
-            <MovieListPage
-              title="Lançamentos"
-              endpoint="/movie/now_playing"
-              fixedSort={{ field: 'release_date', direction: 'desc' }}
-              searchTerm={searchTerm}
-            />
-          }
-        />
-
-        <Route path="/top" element={<MovieListPage title="Top Avaliados" endpoint="/movie/top_rated" searchTerm={searchTerm} />} />
-      </Routes>
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home searchTerm={searchTerm} />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/populares" element={<MovieListPage title="Populares" endpoint="/movie/popular" searchTerm={searchTerm} />} />
+          <Route
+            path="/lancamentos"
+            element={
+              <MovieListPage
+                title="Lançamentos"
+                endpoint="/movie/now_playing"
+                fixedSort={{ field: 'release_date', direction: 'desc' }}
+                searchTerm={searchTerm}
+              />
+            }
+          />
+          <Route path="/top" element={<MovieListPage title="Top Avaliados" endpoint="/movie/top_rated" searchTerm={searchTerm} />} />
+        </Routes>
+      </main>
+      <Footer /> {/* Footer no fim do layout */}
     </>
   );
 }
